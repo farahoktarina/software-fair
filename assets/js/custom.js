9c661998474bd13b53b266873c3369f9999a39b7
Novan <novan.nugi@gmail.com>
2017-03-31 14:50:29 +0700

    background paling atas

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -97,8 +97,8 @@ $(function(){
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
+       "assets/images/home-bg-slideshow1.jpg", 
+       "assets/images/home-bg-slideshow2.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
